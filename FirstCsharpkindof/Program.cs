45e1467d3a4ff5c4f3fb381b9567dff8a5c3ed5b
Luke Barkess <luke.barkess@icloud.com>
2022-09-09 15:02:32 +0100

    comments

--- FirstCsharpkindof/Program.cs
+++ FirstCsharpkindof/Program.cs
@@ -10,15 +10,15 @@ namespace FirstCsharpkindof
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter and epic number");
-            int reply = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter and epic number"); // Tells the user the question for the input
+            int reply = int.Parse(Console.ReadLine()); // Gets the users input and parses it to an integer
 
-            for (int i = 1; i < reply + 1; i++)
+            for (int i = 1; i < reply + 1; i++) // For loop in order to output each number
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i); // Writes the number to the console
             }
 
-            Console.ReadLine();
+            Console.ReadLine(); // So the console doesn't immediatly close
         }
     }
 }
